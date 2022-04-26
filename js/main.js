6c88226273900fbb5a2a10e8c9bd2bd54e8bad15
MINHWAISGOOD <minhwaisgood@naver.com>
2022-04-26 13:38:40 +0900

    4/26 2

--- js/main.js
+++ js/main.js
@@ -30,14 +30,8 @@ tableToggle();
 
 /* ------------------- scroll ------------------- */
 const sections = document.querySelectorAll("section");
-const len = sections.length;
 const lis = document.querySelectorAll("body >ul li");
-
-const btns_arr = Array.from(lis);
-
-let posArr = [];
-const base = -400;
-let enableClick = true;
+const base = -300;
 
 setPos();
 
@@ -48,42 +42,9 @@ window.addEventListener("resize", ()=>{
 
 console.log(posArr);
 
-//마우스 휠을 내리면 li의 값이 변경되면서 해당 section으로 이동하는 이벤트를 걸어줄 것.
-window.addEventListener("mousewheel", (e)=>{
-    // e.preventDefault();
-    // 활성화 되어있는 btns li를 변수로 저장
-    let activeItem = document.querySelector(".btns li.on");
-
-    let activeIndex = btns_arr.indexOf(activeItem);
-    // console.log(activeIndex);
-
-    let targetIndex;
-
-    // 마우스 휠을 올린다.
-    // if문은 인덱스 값만 왔다갔다 해주는 용도
-    if(e.deltaY < 0) {
-        // 마우스 휠을 올리는 경우 ( 음수 )
-        // 3 2 1 0 return;
-        if(activeIndex == 0) return;
-        targetIndex = activeIndex - 1;
-    }else {
-        // 마우스 휠을 내렸을 경우 ( 양수 )
-        // 0 1 2 3 return;
-        if(activeIndex == len - 1) return;
-        targetIndex = activeIndex + 1;
-    }
-    // 마우스휠 이벤트 결과로 나오는 정확한 인덱스 값을 추적하는 코드
-
-    // 결과적으로 anime 쪽에 posArr의 인덱스에 정확한 targetIndex를 주기 위한 것.
-    new Anime(window, {
-        prop: "scroll",
-        value: posArr[targetIndex],
-        duration: 500
-    })
-}, {passive:false})
-
 window.addEventListener("scroll", ()=>{
     let scroll = window.scrollY || window.pageYOffset;
+
     sections.forEach((el,index)=>{
         if(scroll >= posArr[index] + base) {
             lis.forEach((el,index)=>{
@@ -96,25 +57,17 @@ window.addEventListener("scroll", ()=>{
     })
 })
 
-lis.forEach((el,index)=>{
-    el.addEventListener("click", (e)=>{
-        // 클릭한 버튼이 활성화 되어있는지 판별하는 변수를 만든다.
-        let isOn = e.currentTarget.classList.contains("on");
-        // 활성화 되어있다면 종료
-        if(isOn) return; 
-
-        if(enableClick) {
-            
-            new Anime(window, {
-                prop: "scroll",
-                value: posArr[index],
-                duration: 500,
-                callback: ()=>{
-                    enableClick = true;
-                }
-            })
-            enableClick = false;
-        }        
+lis.forEach((li,index)=>{
+    li.addEventListener("click", (e)=>{
+        new Anime(window, {
+            prop: "scroll",
+            value: posArr[index],
+            duration: 500
+        });
+        for(let el of lis) {
+            el.classList.remove("on");
+        }       
+        e.currentTarget.classList.add("on");
     })
 })
 
@@ -131,7 +84,7 @@ function setPos() {
 var swiper = new Swiper(".mySwiper", {
     direction:"horizontal", //슬라이딩 방향 
     slidesPerView: 4,//하나의 화면당 보일 패널의 갯수 
-    spaceBetween: 15,//슬라이드 간 사이간격
+    spaceBetween: 30,//슬라이드 간 사이간격
     loop: true,//슬라이딩 순환의 여부 결정
     grabCursor:true, //스와이프시 마우스 커서 모양
     autoplay:{
---
+++ js/validation.js
@@ -0,0 +1,91 @@
+const form = document.querySelector("#joinForm");
+const btnSubmit = form.querySelector("input[type=submit]");
+
+btnSubmit.addEventListener("click",(e)=>{
+    if(!isText("id", 5)) e.preventDefault(); // id 인증
+    if(!isText("comments",20)) e.preventDefault(); // comments인증
+    if(!isCheck("gender")) e.preventDefault(); // gender 인증
+    if(!isCheck("interest")) e.preventDefault(); // interest 인증
+    if(!isPwd("password1", "password2", 7)) e.preventDefault(); // password인증    
+});
+
+// input-type : text
+function isText(name, len) {
+    //만약 입력반은 len값이 비어있다면 디폴트 값은 5
+    if(len === undefined) {
+        len =5;
+    }
+
+    let input = form.querySelector(`[name=${name}]`);
+    let txt = input.value;
+    
+    if(txt.length >= len) {
+        const errMsgs = input.closest("td").querySelectorAll("p");
+        if(errMsgs.length>0) input.closest("td").querySelector("p").remove();
+
+        return true;
+    }else {        
+        const errMsgs = input.closest("td").querySelectorAll("p");
+        if(errMsgs.length>0) input.closest("td").querySelector("p").remove();
+
+        const errMsg = document.createElement("p");
+        errMsg.append(`입력항목을 ${len}글자 이상 입력하세요.`);
+        input.closest("td").append(errMsg);
+
+        return false;
+    }
+}
+
+function isCheck(name) {
+    let inputs = form.querySelectorAll(`[name=${name}]`);
+    let isCheck = false;
+
+    for(let el of inputs) {
+        if(el.checked) isCheck = true;
+    }
+
+    if(isCheck) {
+        const errMsgs = inputs[0].closest("td").querySelectorAll("p");
+        if(errMsgs.length>0) inputs[0].closest("td").querySelector("p").remove();
+
+        return true;
+    }else {
+        const errMsgs = inputs[0].closest("td").querySelectorAll("p");
+        if(errMsgs.length>0) inputs[0].closest("td").querySelector("p").remove();
+
+        const errMsg = document.createElement("p");
+        errMsg.append("필수 입력 항목을 체크해 주세요.");
+        inputs[0].closest("td").append(errMsg);
+
+        return false;
+    }
+}
+
+function isPwd(name1, name2, len){
+    let pwd1 = form.querySelector(`[name=${name1}]`);
+    let pwd2 = form.querySelector(`[name=${name2}]`);
+
+    let pwd1_val = pwd1.value;
+    let pwd2_val = pwd2.value;
+
+    const num = /[0-9]/;
+    const eng = /[a-zA-Z]/;
+    const spc = /[~!@#$%^&*()_+?><]/;
+
+    if(pwd1_val === pwd2_val && pwd1_val.length >= len && num.test(pwd1_val) && eng.test(pwd1_val) && spc.test(pwd1_val)) {
+        const errMsgs = pwd1.closest("td").querySelectorAll("p");
+        if(errMsgs.length > 0) pwd1.closest("td").querySelector("p").remove();
+
+        return true;
+    }else {
+        const errMsgs = pwd1.closest("td").querySelectorAll("p");
+        if(errMsgs.length >0) pwd1.closest("td").querySelector("p").remove();
+
+        const errMsg = document.createElement("p");        
+        errMsg.append(`비밀번호는 ${len}글자이상, 영문, 숫자, 특수문자를 포함하여 동일하게 입력하세요.`);
+        pwd1.closest("td").append(errMsg);
+
+        return false;
+    }
+}
+
